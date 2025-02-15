eb6a235ed96e508a0c040d338a0001458c1147f9
ARna06 <72038543+Leopard005537@users.noreply.github.com>
2025-02-15 17:08:26 +0530

    implement unembedder

---
+++ src/unembed.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+import torch as t 
+from jaxtyping import Float
+from torch import Tensor 
+import einops 
+from utils.tests import rand_float_test, load_gpt2_test, reference_gpt2, cache
+
+class Unembed(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.W_U = nn.Parameter(t.empty((cfg.d_model, cfg.d_vocab)))
+        nn.init.normal_(self.W_U, std=self.cfg.init_range)
+        self.b_U = nn.Parameter(t.zeros((cfg.d_vocab), requires_grad=False))
+
+    def forward(
+        self, normalized_resid_final: Float[Tensor, "batch position d_model"]
+    ) -> Float[Tensor, "batch position d_vocab"]:
+        return (
+            einops.einsum(
+                normalized_resid_final,
+                self.W_U,
+                "batch posn d_model, d_model d_vocab -> batch posn d_vocab",
+            )
+            + self.b_U
+        )
+
+if __name__ == "__main__":
+    rand_float_test(Unembed, [2, 4, 768])
+    load_gpt2_test(Unembed, reference_gpt2.unembed, cache["ln_final.hook_normalized"])
